eb0096a8e65aed3446ee7667b00f36afedefacb2
iFB24M <fakem1ner@yandex.ru>
2024-03-19 22:05:09 +0400

    add bananatrade

---
+++ src/app/apps/bananatrade/[id]/page.tsx
@@ -0,0 +1,50 @@
+import { Container } from '@/components/Container'
+import styles from './page.module.scss'
+import clsx from 'clsx'
+
+function getMaxOfArray(numArray: number[]) {
+	return Math.max.apply(null, numArray);
+}
+
+const BananaTrade = async ({ params }: { params: { id: number } }) => {
+	const response = await fetch(`https://www.fb24m.ru/tms/wp-json/wp/v2/currencies/${params.id}`)
+	const json: {
+		title: {
+			rendered: string
+		}
+		acf: {
+			rate: {
+				price: number
+			}[]
+		}
+	} = await response.json()
+
+	const prices = json.acf.rate.map(price => price.price)
+
+	const maxSign = getMaxOfArray(prices) + 50
+	let signs: number[] = []
+
+	for (let i = 50; i <= maxSign; i += 50) {
+		signs.push(i)
+	}
+
+	return (
+		<Container>
+			<h2 className={styles.title}>
+				Курс валюты: {json.title.rendered} (MUSD) / р—т (рубль ТМС)
+			</h2>
+			<div className={styles.container}>
+				<div className={styles.prices}>
+					{signs.toReversed().map((price) => <div className={styles.sign} key={price}>{price}</div>)}
+				</div>
+				{prices.map((price, index) =>
+					<div data-title={`${price} р—т (${price - prices[index - 1]})`}
+						className={clsx(styles.column, prices[index - 1] >= price && styles.red)}
+						style={{ height: price }}
+						key={price * index} />)}
+			</div>
+		</Container>
+	)
+}
+
+export default BananaTrade
